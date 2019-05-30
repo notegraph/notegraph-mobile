c9351c3359115c7777770b110389819e06c0dda0
Roman <romitch@gmail.com>
2019-05-29 23:51:39 -0400

    upgraded to latest RN, wip

--- android/app/src/main/java/com/ngkeep/MainApplication.java
+++ android/app/src/main/java/com/ngkeep/MainApplication.java
@@ -1,16 +1,16 @@
 package com.ngkeep;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.chirag.RNMail.RNMail;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.github.xinthink.rnmk.ReactMaterialKitPackage;
-import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
-import com.chirag.RNMail.*;
+import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
 import java.util.List;
@@ -19,7 +19,7 @@ public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
-    protected boolean getUseDeveloperSupport() {
+    public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
     }
 
@@ -27,15 +27,27 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
+            new RNMail(),
             new RNDeviceInfo(),
-          new ReactMaterialKitPackage(),
-          new RNMail()
+            new ReactMaterialKitPackage()
       );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
     }
   };
 
   @Override
   public ReactNativeHost getReactNativeHost() {
-      return mReactNativeHost;
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
   }
 }
